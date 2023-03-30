973bc1c4db040d998d7b38ef02058fb0f4b943bc
jureisgui <111708095+jureisgui@users.noreply.github.com>
2023-03-31 12:13:57 +1300

    user database model

---
+++ back-end/models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+    first_name: {
+        type: String, 
+        required: true},
+    last_name: {
+        type: String, 
+        required: true},
+    email: {
+        type: String,
+        required: true},
+    phone_number: {
+        type: Number,
+        required: true},
+    address: {
+        type: String,
+        required: true},
+    profile_image: {
+        type: String,
+        required: false},
+    seller_name: {
+        type: String,
+        required: false},
+    description: {
+        type: String,
+        required: false},
+    my_listings: {
+        type: Array,
+        required: true},
+
+    });
+// = new mongoose.Schema({fname:String,lname:String})
+const User = mongoose.model("User", UserSchema);
+module.exports = User
