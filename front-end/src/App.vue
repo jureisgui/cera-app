d30db9faa4aac9237678350cf963c45ea7e99dda
kylahiggins <kyla.higgns@outlook.com>
2023-04-04 09:48:17 +1200

    Added Font Library

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,6 +3,8 @@
 </script>
 
 <template>
+    
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 
 
 <!-- Tin's Test Section -->
