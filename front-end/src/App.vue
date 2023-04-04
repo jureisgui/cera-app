0c3582c0784582f551370b594141fc44c81c7099
MattBohan47 <111708097+MattBohan47@users.noreply.github.com>
2023-04-04 12:05:22 +1200

    Button updates

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,30 +1,29 @@
 <script setup>
-
+import MainButton from "./components/Buttons/MainButton.vue";
 </script>
 
 <template>
-    
- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+  />
 
+  <!-- Tin's Test Section -->
 
-<!-- Tin's Test Section -->
+  <!--  -->
 
-<!--  -->
+  <!-- Matt's Test Section -->
+  <MainButton />
+  <!--  -->
 
-<!-- Matt's Test Section -->
+  <!-- Julie's Test Section -->
 
-<!--  -->
+  <!--  -->
 
-<!-- Julie's Test Section -->
+  <!-- Kyla's Test Section -->
 
-<!--  -->
-
-<!-- Kyla's Test Section -->
-
-<!--  -->
- 
+  <!--  -->
 </template>
 
 <style scoped>
-
 </style>
--- front-end/src/components/Buttons/MainButton.vue
+++ front-end/src/components/Buttons/MainButton.vue
@@ -1,13 +1,14 @@
 <template>
-    <div>
-        MAIN BUTTON PLACEHOLDER
+  <button class="button_text main_button_style">
+    <div class="main_button_flex">
+      <span>Log in</span>
+      <span class="material-symbols-outlined"> login </span>
     </div>
+  </button>
 </template>
 
 <style scoped>
-    
 </style>
 
 <script setup>
-
 </script>
