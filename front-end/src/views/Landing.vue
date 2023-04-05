e0049e01b91fc2577aee05264de6edae41c4cf77
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-04-05 13:44:19 +1200

    Added Success modal to Landing.vue with props

--- front-end/src/views/Landing.vue
+++ front-end/src/views/Landing.vue
@@ -1,13 +1,28 @@
 <template>
-  <div class="landing_header">
-   LANDING PLACEHOLDER
-  </div>
+  <Success :success_prop="Log_in" class="success" />
 </template>
 
 <style scoped>
-
+.success {
+  position: absolute;
+  top: 40%;
+  left: 40%;
+  right: 40%;
+  bottom: 50%;
+}
 </style>
 
 <script setup>
+import Success from '../components/Success.vue'
+</script>
 
+<script>
+    export default{
+        data(){
+            return{
+                Sign_up:'Sign Up',
+                Log_in:'Log In'
+            }
+        }
+    }
 </script>
