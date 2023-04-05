a770c5a933587b3a8b9a0a7d42be1b2230007783
kylahiggins <kyla.higgns@outlook.com>
2023-04-06 09:16:21 +1200

    Main Button Build Matt

--- front-end/src/components/Buttons/MainButton.vue
+++ front-end/src/components/Buttons/MainButton.vue
@@ -1,13 +1,33 @@
 <template>
-    <div>
-        MAIN BUTTON PLACEHOLDER
-    </div>
+  <div>
+    <button v-for="(button, index) in buttons" :key="index">
+      {{ button.label }}
+      <i :class="button.icon"></i>
+      <span class="material-symbols-outlined"> logout </span>
+      <i class="fa fa-user" aria-hidden="true"></i>
+    </button>
+  </div>
 </template>
 
-<style scoped>
-    
-</style>
-
-<script setup>
-
+<script>
+export default {
+  data() {
+    return {
+      buttons: [
+        {
+          label: "Button 1",
+          icon: "fa-solid fa-coffee",
+        },
+        {
+          label: "Button 2",
+          icon: "fas fa-search",
+        },
+        {
+          label: "Button 3",
+          icon: "fas fa-envelope",
+        },
+      ],
+    };
+  },
+};
 </script>
