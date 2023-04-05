5a436146d896bbc934f94f2aa0a7c88dd3e5c3a5
jureisgui <111708095+jureisgui@users.noreply.github.com>
2023-04-06 11:38:55 +1200

    message input done
    Prop, styling and responsiveness should be working

--- front-end/src/components/Inputs/MessageInput.vue
+++ front-end/src/components/Inputs/MessageInput.vue
@@ -1,13 +1,36 @@
 <template>
-    <div>
-        MESSAGE INPUT PLACEHOLDER
+    <div> 
+        <textarea class="body_text" rows="6" cols="60" :placeholder="Messageplaceholder"></textarea>
     </div>
 </template>
 
 <style scoped>
+    textarea{
+        box-sizing: border-box;
+        border: 1px solid #C4C4C4;
+        border-radius: 10px;
+        padding: 10px;
+        resize: vertical;
+        width: 100%;
+    }
+
+    textarea::placeholder{
+        color: #cacaca;
+    }
+
+    textarea:focus{
+        outline: 1px solid #DCBA77;
+    }
+
     
 </style>
 
 <script setup>
+
+defineProps({ 
+    Messageplaceholder: { 
+        type: String, 
+        required: true } 
+    });
 
 </script>
