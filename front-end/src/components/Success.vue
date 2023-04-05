343af4f5afe998f9e0bf5be8ec25a0de3a639dd1
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-04-05 13:43:45 +1200

    Created initial success modal with border

--- front-end/src/components/Success.vue
+++ front-end/src/components/Success.vue
@@ -1,13 +1,39 @@
 <template>
-    <div>
-        SUCCESS PLACEHOLDER
+  <div class="modal_parent">
+    <div class="success_modal_div">
+      <span class="material-symbols-outlined">check_circle</span>
+      <h3 class="input_label">{{ success_prop }} Success!</h3>
     </div>
+  </div>
 </template>
 
 <style scoped>
-    
+.modal_parent {
+    position: relative;
+    background-color: #fff;
+}
+.success_modal_div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  border: 1px solid black;
+  padding: 20px;
+  width: 30vw;
+  height: 20vw;
+}
+
+.material-symbols-outlined {
+  color: #44b91b;
+}
 </style>
 
 <script setup>
-
+defineProps({
+  success_prop: {
+    type: String,
+    required: true,
+  },
+});
 </script>
