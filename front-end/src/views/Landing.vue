3d4c4d229330e6906c859ef6e600626434f17e8d
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-04-06 09:08:51 +1200

    Removed content (unnecessary)

--- front-end/src/views/Landing.vue
+++ front-end/src/views/Landing.vue
@@ -1,28 +1,13 @@
 <template>
-  <!-- <Success :success_prop="Log_in" class="success" @close_modal="" /> -->
+  LANDING PLACEHOLDER
 </template>
 
 <style scoped>
-/* .success {
-  position: absolute;
-  top: 40%;
-  left: 35%;
-  right: 35%;
-  bottom: 50%;
-} */
+
 </style>
 
 <script setup>
-import Success from '../components/Success.vue'
+
 </script>
 
-<script>
-    export default{
-        data(){
-            return{
-                Sign_up:'Sign Up',
-                Log_in:'Log In'
-            }
-        }
-    }
-</script>
+
