c1719113c97f8a7ef0a86d8fd4720c653ee7cca6
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-04-06 09:08:40 +1200

    Removed things from App.vue before merging

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,11 +1,6 @@
 <script setup>
 
-import Categories from './components/Categories.vue'
-import Header from './components/Header.vue'
-import LongButton from './components/Buttons/LongButton.vue'
-import sort_by_button from './components/Buttons/Sortby.vue'
-import TransparentHeader from './components/TransparentHeader.vue'
-import Success from './components/Success.vue'
+
 
 </script>
 
@@ -15,8 +10,7 @@ import Success from './components/Success.vue'
 
 
 <!-- Tin's Test Section -->
-<Header />
-<Success :success_prop="Log_in" class="success" @close_modal="" />
+
 <!--  -->
 
 <!-- Matt's Test Section -->
@@ -35,22 +29,9 @@ import Success from './components/Success.vue'
 </template>
 
 <style scoped>
-.success {
-  position: absolute;
-  top: 40%;
-  left: 35%;
-  right: 35%;
-  bottom: 50%;
-}
+
 </style>
 
 <script>
-    export default{
-        data(){
-            return{
-                Sign_up:'Sign Up',
-                Log_in:'Log In'
-            }
-        }
-    }
+  
 </script>
