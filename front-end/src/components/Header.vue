c6bc4146663e2a8f4088700f00a4218bef041f9d
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-04-06 10:33:57 +1200

    Removed unnecessary class

--- front-end/src/components/Header.vue
+++ front-end/src/components/Header.vue
@@ -26,8 +26,8 @@
       <span class="material-symbols-outlined" v-if="Logged_in">
         account_circle
       </span>
-      <MainBtn main_button_prop="Log in" button_icon_prop="login" class="outline_btn" v-if="!Logged_in"/>
-      <MainBtn main_button_prop="List an item" button_icon_prop="add_circle" class="outline_btn" v-if="Logged_in"/>
+      <MainBtn main_button_prop="Log in" button_icon_prop="login" v-if="!Logged_in"/>
+      <MainBtn main_button_prop="List an item" button_icon_prop="add_circle" v-if="Logged_in"/>
     </div>
 
     <div>
