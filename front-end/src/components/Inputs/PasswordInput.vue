7513b5aa72741d956eb08c86f8f8c3885c6f2ce0
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-04-07 16:43:09 +1200

    Created PasswordInput.vue

---
+++ front-end/src/components/Inputs/PasswordInput.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="input_container">
+        <input type="password" :placeholder="Inputplaceholder">
+
+    </div>
+</template>
+
+<style scoped>
+
+.input_container{
+    display: flex;
+
+}
+
+input{
+    display: flex;
+    width: 100%;
+    height: 60px;
+    border-radius: 10px;
+    border: 1px solid #c4c4c4;
+    padding: 10px;
+}
+
+
+textarea::placeholder { 
+    color: #cacaca; 
+}
+    
+</style>
+
+<script setup>
+defineProps({
+    Inputplaceholder: {
+    type: String,
+    required: true
+  }
+});
+
+</script>
