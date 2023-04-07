5250625f38fbc23d005adad34378ff8d29373fd0
jureisgui <111708095+jureisgui@users.noreply.github.com>
2023-04-07 15:27:45 +1200

    modal almost done
    modal needs to be in the middle of the screen, will come back to it to review the logic as well

--- front-end/src/components/Modals/ContactSellerModal/ContactSeller.vue
+++ front-end/src/components/Modals/ContactSellerModal/ContactSeller.vue
@@ -3,17 +3,19 @@
         <div class="contact_seller_modal">
             <span class="material-symbols-outlined close_icon" @click="$emit('close_modal')"> close </span>
             <div class="seller_info_wrapper">
-                <div class="seller_thumbnail">
-                    <img src="" alt="">                
+                <div>
+                    <!-- <img :src="seller_image" alt="Seller thumbnail"> --> 
+                    <!-- Placeholder below -->
+                    <img class="seller_thumbnail" src="@/assets/img/Person.png" alt="Seller Thumbnail">                
                 </div>
                 <div class="seller_info">
                     <h1 class="heading_text">{{ seller_name }} Cafe Concetto</h1>
                     <p class="body_text">{{ location }}Christchurch, New Zealand</p>
-                    <span class="material-symbols-outlined">star_rate</span>
-                    <span class="material-symbols-outlined">star_rate</span>
-                    <span class="material-symbols-outlined">star_rate</span>
-                    <span class="material-symbols-outlined">star_rate</span>
-                    <span class="material-symbols-outlined">star_rate</span>                
+                    <span class="material-symbols-outlined star_filled">star_rate</span>
+                    <span class="material-symbols-outlined star_filled">star_rate</span>
+                    <span class="material-symbols-outlined star_filled">star_rate</span>
+                    <span class="material-symbols-outlined star_filled">star_rate</span>
+                    <span class="material-symbols-outlined star_unfilled">star_rate</span>                
                 </div>            
             </div>
             <h2 class="subheading_text">Purchase Inquiry for:</h2>
@@ -23,11 +25,13 @@
                 <p class="body_text">{{ email }}jane-doe@email.com</p>            
             </div>
             <MessageInput Messageplaceholder="Type your message here"/>
+            <MainButton main_button_prop="Contact Seller" button_icon_prop="mail" />
         </div>
   </div>
 </template>
 
 <style scoped>
+
 .modal_parent {
     position: relative;
     background-color: #fff;
@@ -36,14 +40,12 @@
 .contact_seller_modal{
     display: flex;
     flex-direction: column;
-    /* align-items: center; */
-    /* justify-content: center; */
-    gap: 20px;
+    gap: 30px;
     border: 1px solid black;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    padding: 20px;
-    width: 50vw;
-    height: 40vw;
+    padding: 10%;
+    width: 60vw;
+    /* height: 50vw; */
     Top: 160px; 
     Left: 450px;
 }
@@ -59,11 +61,47 @@
     cursor: pointer;
     transform: scale(1.1);
 }
+
+.seller_info_wrapper{
+    margin: 0 auto;
+    display: flex;
+    gap: 10px;
+}
+
+.seller_thumbnail{
+    width: 115px;
+    height: 115px;
+    border-radius: 50%;
+    border: solid 0.5px #7D7D7D;
+}
+
+
+.material-symbols-outlined {
+  font-variation-settings:
+  'FILL' 1,
+  'wght' 400,
+  'GRAD' 0,
+  'opsz' 48
+}
+
+.star_filled{
+    color: #DCBA77;
+}
+
+.star_unfilled{
+    color: #E4E4E4;
+}
+
+.main_button{
+    margin: 0 auto;
+    background-color: #DC7777;
+}
     
 </style>
 
 <script setup>
 import MessageInput from '@/components/Inputs/MessageInput.vue'
+import MainButton from '@/components/Buttons/MainButton.vue'
 
 defineProps({
   success_prop: {
