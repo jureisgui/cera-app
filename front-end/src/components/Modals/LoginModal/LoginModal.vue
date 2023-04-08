e08640e7ffb0eccf78ac64e5b2c708b6095cb739
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-04-08 22:29:39 +1200

    Updated styling in Login modal

--- front-end/src/components/Modals/LoginModal/LoginModal.vue
+++ front-end/src/components/Modals/LoginModal/LoginModal.vue
@@ -12,11 +12,13 @@
         <p class="body_text">Hello Pottery People! Let's get logged in!</p>
       </div>
       <div class="form body_text">
-        <p class="error_msg">
-          Log-in failed. Please enter correct email or password.
-        </p>
-        <MainInput Inputplaceholder="Email" />
-        
+        <div class="input_row">
+          <p class="error_msg">
+            Log-in failed. Please enter correct email or password.
+          </p>
+          <MainInput Inputplaceholder="Email" />
+        </div>
+        <PasswordInput Password_placeholder="Password" />
         <LongBtn long_button_text="Log in" />
       </div>
       <div class="bottom_text body_text">
@@ -45,7 +47,8 @@
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 20px;
+  gap: 40px;
+  padding: 0 3em;
 }
 
 .close_icon {
@@ -82,10 +85,14 @@
   color: #db1717;
   font-size: 12px;
 }
+
+.login_title {
+    text-align: center;
+}
 </style>
 
 <script setup>
 import LongBtn from "../../Buttons/LongButton.vue";
 import MainInput from "../../Inputs/MainInput.vue";
-
+import PasswordInput from "../../Inputs/PasswordInput.vue";
 </script>
