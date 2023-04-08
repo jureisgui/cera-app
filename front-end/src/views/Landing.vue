6338294ddfa48b7a36f8fa83bb58f928e12a5346
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-04-08 20:48:08 +1200

    Added bg fade when modal on in Landing

--- front-end/src/views/Landing.vue
+++ front-end/src/views/Landing.vue
@@ -1,22 +1,43 @@
 <template>
-<Hero />
-<Summary />
-<Categories />
-<FeaturedArtist />
+  <section class="landing">
+    <Header Header_class="transparent" Logo="src/assets/img/pine_logo-01.svg" />
+    <div class="blur"></div>
+    <Hero />
+    <Summary />
+    <Categories />
+    <FeaturedArtist />
+
+    <Login class="login" />
+  </section>
 </template>
 
 <style scoped>
+.landing {
+  position: relative;
+}
 
+.login {
+  position: absolute;
+  top: 105px;
+  z-index: 1000;
+}
+
+.blur {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 998;
+  background: rgba(0, 0, 0, 0.3);
+}
 </style>
 
 <script setup>
-import Header from '../components/Header.vue'
-import Hero from '../components/LandingComponents/HeroSection.vue'
-import Summary from '../components/LandingComponents/Summary.vue'
-import FeaturedArtist from '../components/LandingComponents/FeaturedArtist.vue'
-import Categories from '../components/Categories.vue'
+import Header from "../components/Header.vue";
+import Hero from "../components/LandingComponents/HeroSection.vue";
+import Summary from "../components/LandingComponents/Summary.vue";
+import FeaturedArtist from "../components/LandingComponents/FeaturedArtist.vue";
+import Categories from "../components/Categories.vue";
+import Login from "../components/Modals/LoginModal/LoginModal.vue";
 </script>
-
-
-
-
