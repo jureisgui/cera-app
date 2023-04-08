c6c8914d3cf0babad999ea76afb5e5848db8f636
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-04-08 20:47:38 +1200

    created Login modal

--- front-end/src/components/Modals/LoginModal/LoginModal.vue
+++ front-end/src/components/Modals/LoginModal/LoginModal.vue
@@ -1,11 +1,24 @@
 <template>
-    <div>
-        LOGIN MODAL PLACEHOLDER
+ <div class="modal_parent">
+    <div class="login_modal_div">
+        <div class="login_title">
+        <h2 class="subheading_text">Log in</h2>
+        <p class="body_text">
+            Hello Pottery People! Let's get logged in!
+        </p>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
-    
+   
+    .login_modal_div {
+        background-color: #fff;
+        width: 50vw;
+        position: fixed;
+        top: 105px;
+    }
 </style>
 
 <script setup>
