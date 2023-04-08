29fb7224660e45dca9dcc66efc2844c4911bbb1f
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-04-08 20:48:43 +1200

    Update SignUp.vue

--- front-end/src/views/SignUp.vue
+++ front-end/src/views/SignUp.vue
@@ -1,22 +1,24 @@
 <template>
-  <Header Header_class="green" Logo="src/assets/img/white_logo-01-01.svg" />
-SIGNUP
+  <section class="modal_parent">
+    <Header Header_class="green" Logo="src/assets/img/white_logo-01-01.svg" />
+  </section>
+  <div></div>
 </template>
 
 <style scoped>
-    
+.modal_parent {
+    position: relative;
+}
 </style>
 
 <script setup>
-import Header from '../components/Header.vue'
+import Header from "../components/Header.vue";
 </script>
 
 <script>
 export default {
   data() {
-    return {
-      
-    };
+    return {};
   },
 };
 </script>
