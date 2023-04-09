441eef94f36a7f29edc959dbb919f11e29acebcb
jureisgui <111708095+jureisgui@users.noreply.github.com>
2023-04-10 11:15:59 +1200

    test

--- front-end/src/components/Modals/AccountModal/AccountModal.vue
+++ front-end/src/components/Modals/AccountModal/AccountModal.vue
@@ -1,69 +1,23 @@
-<script setup>
-import MyInfo from './MyInfo.vue'
-import PublicProfile from './PublicProfile.vue'
-</script>
-
 <template>
-    <div>
-      <div v-for="(item, index) in items" :key="index" class="accordion-item">
-        <div class="accordion-header" @click="toggleAccordion(index)">
-          <span class="heading_text">{{ item.title }}</span>
-          <span :class="{ 'active': activeAccordion === index }">&#9660;</span>
-        </div>
-        <div
-          class="accordion-content body_text"
-          :class="{ 'active': activeAccordion === index }">
-          {{ item.content }}
-        </div>
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        activeAccordion: null,
-        items: [
-          { title: "My Information", 
-          content: "MyInfo" },
-          { title: "My Public Profile (optional - this is what buyers will see when they view your profile)", 
-          content: "Content for Item 2" },
-          { title: "My Listings", 
-          content: "Content for Item 3" }
-        ]
-      };
-    },
-    methods: {
-      toggleAccordion(index) {
-        if (this.activeAccordion === index) {
-          this.activeAccordion = null;
-        } else {
-          this.activeAccordion = index;
-        }
-      }
-    }
-  };
-  </script>
-  
-  <style scoped>
-  .accordion-item {
-    border: 1px solid #ccc;
-    margin-bottom: 10px;
+  <div>
+    <Accordion title="Accordion 1">
+      <p>Content for Accordion 1</p>
+    </Accordion>
+    <Accordion title="Accordion 2">
+      <p>Content for Accordion 2</p>
+    </Accordion>
+    <Accordion title="Accordion 3">
+      <p>Content for Accordion 3</p>
+    </Accordion>
+  </div>
+</template>
+
+<script>
+import Accordion from './Accordion.vue';
+
+export default {
+  components: {
+    Accordion
   }
-  
-  .accordion-header {
-    cursor: pointer;
-    padding: 10px;
-  }
-  
-  .accordion-content {
-    padding: 10px;
-    display: none;
-  }
-  
-  .active {
-    display: block;
-  }
-  </style>
-  
+};
+</script>
