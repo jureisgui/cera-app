7cc45bed0b49a0caa1e160b0cd2541a2ad5f4cd6
kylahiggins <kyla.higgns@outlook.com>
2023-04-10 10:53:16 +1200

    Listing Tags Start

---
+++ front-end/src/components/Inputs/InputWIthButton.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="input_container">
+    <input type="text" :placeholder="Inputplaceholder"/>
+    <span class="material-symbols-outlined">add</span>
+  </div>
+</template>
+
+<style scoped>
+.input_container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  border-radius: 10px;
+}
+
+input {
+  display: flex;
+  min-width: 300px;
+  width: 100%;
+  height: 60px;
+  border-radius: 10px;
+  border: none;
+  padding: 10px;
+}
+
+input:focus{
+    outline: none;
+}
+
+textarea::placeholder {
+  color: #cacaca;
+}
+
+input .material-symbols-outlined{
+  position: absolute;
+  left: 100px;
+}
+</style>
+
+<script setup>
+defineProps({
+  Inputplaceholder: {
+    type: String,
+    required: true,
+  },
+});
+</script>
--- front-end/src/components/Modals/AddListingModal/ListingTags.vue
+++ front-end/src/components/Modals/AddListingModal/ListingTags.vue
@@ -1,13 +1,54 @@
 <template>
-    <div>
-        LISTING TAGS PLACEHOLDER
+  <div class="Listing_Info_Container body_text">
+    <div class="ListingInfoHeading">
+      <h1 class="subheading_text">Product Tags<hr/></h1>
     </div>
+
+    <div class="product_tag_container">
+    
+        <div class="tags_title">
+            <div>
+                <div><label for=""> <span style="color: red;">*</span>Write a description for your listing.</label></div>
+                <div><label for="">(You may add up to 10 tags)</label></div>
+            </div>
+        </div>
+
+        <div class="product_tag_input">
+        <InputWithButton/>
+        
+        
+        
+        </div>
+
+    </div>
+  </div>
 </template>
 
 <style scoped>
-    
+
+
+
+.Listing_Info_Container {
+  margin-top: 2em;
+  border: solid 1px #444739;
+  padding: 2em;
+}
+
+.tags_title{
+   margin: 3em 0 2em;
+    display: flex;
+    justify-content: center;
+    text-align: center;
+}
+
+.product_tag_input{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
 </style>
 
 <script setup>
-
+import InputWithButton from '../../Inputs/InputWIthButton.vue'
 </script>
