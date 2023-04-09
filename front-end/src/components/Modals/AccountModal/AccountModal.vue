b354da8bcac753a4f5ce27ec6efd6b1894723ae0
jureisgui <111708095+jureisgui@users.noreply.github.com>
2023-04-10 11:51:59 +1200

    accordion working, needs styling

--- front-end/src/components/Modals/AccountModal/AccountModal.vue
+++ front-end/src/components/Modals/AccountModal/AccountModal.vue
@@ -1,23 +1,43 @@
+<script setup>
+//theme
+// import "primevue/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primevue/resources/primevue.min.css";
+
+//icons
+import "primeicons/primeicons.css";
+
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
+import MyInfo from './MyInfo.vue'
+import PublicProfile from './PublicProfile.vue'
+
+</script>
+
+    
 <template>
-  <div>
-    <Accordion title="Accordion 1">
-      <p>Content for Accordion 1</p>
-    </Accordion>
-    <Accordion title="Accordion 2">
-      <p>Content for Accordion 2</p>
-    </Accordion>
-    <Accordion title="Accordion 3">
-      <p>Content for Accordion 3</p>
-    </Accordion>
-  </div>
+    <div class="card">
+        <Accordion :multiple="true" :activeIndex="[0]">
+            <AccordionTab header="My Information">
+                <section><MyInfo /></section>
+            </AccordionTab>
+            <AccordionTab header="My Public Profile (optional - this is what buyers will see when they view your profile)">
+              <section><PublicProfile /></section>
+            </AccordionTab>
+            <AccordionTab header="Header III">
+                <p>
+                    At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui
+                    officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+                </p>
+            </AccordionTab>
+        </Accordion>
+    </div>
 </template>
 
-<script>
-import Accordion from './Accordion.vue';
+<style>
+body {
+    font-family: (--font-family);
+}
 
-export default {
-  components: {
-    Accordion
-  }
-};
-</script>
+</style>
