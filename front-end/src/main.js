3344aa040ec79fbb84b69c00468f717bf3407a01
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-04-10 11:34:44 +1200

    Added gallery using PrimeVue

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css"
+import PrimeVue from 'primevue/config';
 createApp(App)
   .use(router)
+  .use(PrimeVue)
   .mount("#app");
