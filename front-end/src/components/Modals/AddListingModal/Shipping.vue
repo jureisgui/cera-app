389f0642ff2324d99d269b1aa4d89d2d4246404a
kylahiggins <kyla.higgns@outlook.com>
2023-04-10 13:31:26 +1200

    Shipping Component visuality done

--- front-end/src/components/Modals/AddListingModal/Shipping.vue
+++ front-end/src/components/Modals/AddListingModal/Shipping.vue
@@ -1,13 +1,55 @@
 <template>
+  <div class="Listing_Info_Container body_text">
     <div>
-        SHIPPING PLACEHOLDER
+      <h1 class="subheading_text">Shipping & Pickup<hr/></h1>
     </div>
+
+    <div class="shipping_container_wrapper">
+    <div class="shipping_container">
+        <div class="OptionTitle"><span>Choose a Pickup Option<sup>*</sup></span></div>
+        <div class="shipping_option"><input type="radio">No Pickup</div>
+        <div class="shipping_option"><input type="radio">Pickup Available</div>
+        <div class="shipping_option"><input type="radio">Must Pickup</div>
+    </div>
+    <div class="shipping_container">
+        <div class="OptionTitle"><span>Choose a Shipping Option<sup>*</sup></span></div>
+        <div class="shipping_option"><input type="radio">Free Shipping</div>
+        <div class="shipping_option"><input type="radio">Specify Shipping Costs</div>
+        <div class="shipping_option"><input type="radio">I dont know yet</div>
+    </div>
+  </div>
+    
+
+  </div>
 </template>
 
 <style scoped>
-    
-</style>
 
-<script setup>
+sup{
+    color: red;
+}
+  .Listing_Info_Container{
+        margin-top: 2em;
+        border: solid 1px #444739;
+        padding: 2em;
+    }
 
-</script>
+    .shipping_container_wrapper{
+        margin: 2em 20%;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .shipping_container{
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+
+    .shipping_option{
+        display: flex;
+        gap: 10px;
+    }
+    </style>
+
+<script setup></script>
