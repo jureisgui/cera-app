4f8675c37d480e512a895d82dd7ca230f9ac1d21
jureisgui <111708095+jureisgui@users.noreply.github.com>
2023-04-10 12:11:34 +1200

    Update AccountModal.vue

--- front-end/src/components/Modals/AccountModal/AccountModal.vue
+++ front-end/src/components/Modals/AccountModal/AccountModal.vue
@@ -4,66 +4,14 @@ import PublicProfile from './PublicProfile.vue'
 </script>
 
 <template>
-    <div>
-      <div v-for="(item, index) in items" :key="index" class="accordion-item">
-        <div class="accordion-header" @click="toggleAccordion(index)">
-          <span class="heading_text">{{ item.title }}</span>
-          <span :class="{ 'active': activeAccordion === index }">&#9660;</span>
-        </div>
-        <div
-          class="accordion-content body_text"
-          :class="{ 'active': activeAccordion === index }">
-          {{ item.content }}
-        </div>
-      </div>
-    </div>
+    placeholder
   </template>
   
   <script>
-  export default {
-    data() {
-      return {
-        activeAccordion: null,
-        items: [
-          { title: "My Information", 
-          content: "MyInfo" },
-          { title: "My Public Profile (optional - this is what buyers will see when they view your profile)", 
-          content: "Content for Item 2" },
-          { title: "My Listings", 
-          content: "Content for Item 3" }
-        ]
-      };
-    },
-    methods: {
-      toggleAccordion(index) {
-        if (this.activeAccordion === index) {
-          this.activeAccordion = null;
-        } else {
-          this.activeAccordion = index;
-        }
-      }
-    }
-  };
+  
   </script>
   
   <style scoped>
-  .accordion-item {
-    border: 1px solid #ccc;
-    margin-bottom: 10px;
-  }
   
-  .accordion-header {
-    cursor: pointer;
-    padding: 10px;
-  }
-  
-  .accordion-content {
-    padding: 10px;
-    display: none;
-  }
-  
-  .active {
-    display: block;
-  }
   </style>
   
