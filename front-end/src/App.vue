bbda4b4eb68cf18959602227173ae5ba6b185816
MattBohan47 <111708097+MattBohan47@users.noreply.github.com>
2023-04-12 11:43:32 +1200

    Warping circle fix

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,54 +1,48 @@
 <script setup>
-
-import Footer from './components/Footer.vue'
+import Footer from "./components/Footer.vue";
 
 import ProductModal from "./components/Modals/ProductModal/ProductModal.vue";
-
-
 </script>
 
 <template>
   <!-- Google Icons -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+  />
 
   <!-- Tin's Test Section -->
 
   <!--  -->
 
   <!-- Matt's Test Section -->
-
+  <ProductModal />
   <!--  -->
 
   <!-- Julie's Test Section -->
-
 
   <!--  -->
 
   <!-- Kyla's Test Section -->
   <!--  -->
 
-  <router-view></router-view>
+  <!-- <router-view></router-view> -->
   <!-- <Footer /> -->
 </template>
 
 <style scoped>
-
 </style>
 
 <script>
-  export default {
-  data(){
-    return{
-      Logged_in: false
-    }
+export default {
+  data() {
+    return {
+      Logged_in: false,
+    };
   },
-  methods:{
-    
-  },
-  created(){
-    
-  }
-}
+  methods: {},
+  created() {},
+};
 </script>
 
 <style scoped></style>
