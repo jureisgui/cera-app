ca7369c1eb1634ccea6043fb1c7ca26887fa6753
jureisgui <111708095+jureisgui@users.noreply.github.com>
2023-04-12 20:01:13 +1200

    inputs and back end updated

--- front-end/src/components/Modals/AccountModal/MyInfo.vue
+++ front-end/src/components/Modals/AccountModal/MyInfo.vue
@@ -76,6 +76,15 @@
   padding: 10px;
 }
 
+textarea{
+        box-sizing: border-box;
+        border: 1px solid #C4C4C4;
+        border-radius: 10px;
+        padding: 10px;
+        width: 100%;
+        height: 100px;
+    }
+
 input:focus{
     outline: 1px solid #DCBA77;
 }
