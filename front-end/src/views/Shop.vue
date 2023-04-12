1843f1c053843f32bf575f016e8629488920686e
kylahiggins <kyla.higgns@outlook.com>
2023-04-12 23:05:24 +1200

    Added grid to shop and fixed a couple responsiveness issues

--- front-end/src/views/Shop.vue
+++ front-end/src/views/Shop.vue
@@ -10,8 +10,8 @@ import ProductCard from "../components/ShopComponents/ProductCard.vue";
     <Header Header_class="green" Logo="src/assets/img/white_logo-01-01.svg" />
     <SortBy />
     <Categories />
-    <div class="shop-section"></div>
-    <h1 class="heading_text">Shop</h1><!-- Filter for title needs to be added -->
+    <div class="shop-section">
+      <h1 class="heading_text">Shop</h1><!-- Filter for title needs to be added -->
     <div class="products_container">
         <ProductCard />
         <ProductCard />
@@ -20,31 +20,38 @@ import ProductCard from "../components/ShopComponents/ProductCard.vue";
         <ProductCard />
         <ProductCard />    
     </div>
+    </div>
+    
 
 </template>
 
 <style scoped>
 
+.shop-section{
+  margin: 0 10%;
+}
+
 .heading_text {
   color: #212121;
   margin-top: 1.2em;
   margin-bottom: 2em;
-  margin-left: 10%;
-  margin-right: 10%;
 }
 
 .products_container{    
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    margin-left: 10%;
-    margin-right: 10%;
-    margin-bottom: 2em;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    margin-bottom: 2em; 
 }
 
 @media (max-width: 769px) {
   .products_container {
-    justify-content: center;
+    grid-template-columns: 1fr 1fr; 
+  }
+}
+
+@media (max-width: 500px) {
+  .products_container {
+    grid-template-columns: 1fr; 
   }
 }
 
