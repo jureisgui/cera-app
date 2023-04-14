e4181109d56892dd6b20f8bec16ed2a64568e269
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-04-15 10:39:47 +1200

    Product modal showing on click

--- front-end/src/views/Shop.vue
+++ front-end/src/views/Shop.vue
@@ -10,39 +10,51 @@ defineProps({
   Logged_in: Boolean,
   logged_user_obj: Object,
 });
-
 </script>
 
 <template>
-    <Header Header_class="green" Logo="src/assets/img/white_logo-01-01.svg" 
-    @show_login_modal="login_modal=true" :Logged_in="Logged_in"  />
-    <SortBy />
-    <Categories />
-    <h1 class="heading_text">Shop</h1><!-- Filter for title needs to be added -->
-    <div class="shop-section">
-        <div v-for="listing in ListingsArray" class="products_container">
-          <div class="image_wrapper">
-            <img class="product_image" :src="listing.product_images" alt="Product Thumbnail">
-            <Heart class="heart_position" />
-          </div>
-          <h1 class="subheading_text">{{ listing.title }}</h1>
-          <p class="product_cost">${{ listing.price }} NZD</p>
-        </div>
+  <Header
+    Header_class="green"
+    Logo="src/assets/img/white_logo-01-01.svg"
+    @show_login_modal="login_modal = true"
+    :Logged_in="Logged_in"
+  />
+  <SortBy />
+  <Categories />
+  <!-- Filter for title needs to be added -->
+  <div class="shop-section">
+    <div
+      v-for="listing in ListingsArray"
+      class="products_container"
+      @click="product_modal_on = true"
+    >
+      <div class="image_wrapper">
+        <img
+          class="product_image"
+          :src="listing.product_images"
+          alt="Product Thumbnail"
+        />
+        <Heart class="heart_position" />
+      </div>
+      <h1 class="subheading_text">{{ listing.title }}</h1>
+      <p class="product_cost">${{ listing.price }} NZD</p>
     </div>
-    
-
+  </div>
+  <ProductModal
+    v-if="product_modal_on"
+    @CloseModal="product_modal_on = false" class="product_modal_css"
+  />
 </template>
 
 <style scoped>
-
-.heading_text{
+.heading_text {
   margin: 0 10%;
   color: #212121;
   margin-top: 1.2em;
   margin-bottom: 2em;
-} 
+}
 
-.products_container{
+.products_container {
   text-align: center;
   padding: 1em;
 }
@@ -60,8 +72,8 @@ defineProps({
   line-height: 30px;
 }
 
-.shop-section{
-  margin: 0 10%;
+.shop-section {
+  margin: 5% 10%;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   margin-bottom: 2em;
@@ -73,10 +85,10 @@ defineProps({
   position: relative;
 }
 
-.heart_position{
+.heart_position {
   position: absolute;
   top: 10px;
-  right: 20px
+  right: 20px;
 }
 
 .products_container:hover {
@@ -84,63 +96,71 @@ defineProps({
   cursor: pointer;
 }
 
+.product_modal_css {
+  position: absolute;
+  background-color: white;
+  width: 100%;
+  top: 105px;
+  z-index: 10;
+}
 
 @media (max-width: 769px) {
   .shop-section {
-    grid-template-columns: 1fr 1fr; 
+    grid-template-columns: 1fr 1fr;
   }
 }
 
 @media (max-width: 500px) {
   .shop-section {
-    grid-template-columns: 1fr; 
+    grid-template-columns: 1fr;
   }
 }
-
 </style>
 
 <script>
-export default { 
-  data() { 
-    return { 
-      ListingsArray:[],
+export default {
+  data() {
+    return {
+      ListingsArray: [],
       product_body_data: {
         title: "",
         subtitle: "",
         category: "",
         item_description: "",
         condition: {},
-        dimensions: { 
-          width: "", 
-          height: "", 
-          diameter: "", 
-          capacity: "" 
+        dimensions: {
+          width: "",
+          height: "",
+          diameter: "",
+          capacity: "",
         },
         price: 0,
         product_tags: [],
         product_images: [],
         shipping: {},
         specify_costs: {},
-        user_id: '',
+        user_id: "",
       },
-
-    } 
-  }, 
-    methods: { 
-      async fetch_all_listings(){ 
-        const response = await fetch("http://localhost:4000/listings/"); 
-        const received_data = await response.json(); 
-        this.ListingsArray=received_data; 
-        console.log(received_data)
-      },
-      async fetch_single_product(listingID){ 
-      const response = await fetch("http://localhost:4000/listings/getlisting/"+listingID); 
-      const received_data = await response.json(); 
-      this.product_body_data=received_data; 
-    }, 
-    }, 
-        created() { 
-          this.fetch_all_listings(); 
-        }, 
-      } 
-      </script>
+      product_modal_on: false,
+    };
+  },
+  methods: {
+    async fetch_all_listings() {
+      const response = await fetch("http://localhost:4000/listings/");
+      const received_data = await response.json();
+      this.ListingsArray = received_data;
+      console.log(received_data);
+    },
+    async fetch_single_product(listingID) {
+      const response = await fetch(
+        "http://localhost:4000/listings/getlisting/" + listingID
+      );
+      const received_data = await response.json();
+      this.product_body_data = received_data;
+    },
+  },
+  created() {
+    this.fetch_all_listings();
+  },
+};
+</script>
