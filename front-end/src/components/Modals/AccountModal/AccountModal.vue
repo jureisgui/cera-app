e1cc57f0316fc85d5c7cad8655b2fd798730c8bc
Tin Baltazar <57336759+kmrbaltazar@users.noreply.github.com>
2023-04-16 11:09:41 +1200

    Account modal: updated css, added data variables, added update function

--- front-end/src/components/Modals/AccountModal/AccountModal.vue
+++ front-end/src/components/Modals/AccountModal/AccountModal.vue
@@ -15,11 +15,13 @@ import MyListings from "./MyListings.vue";
 defineProps({
   Logged_in: Boolean,
   logged_user_obj: Object,
+  user_account: Object,
 });
 </script>
 
 <template>
   <div v-if="ModalIsOpen" class="modal_wrapper">
+  <br><br>
     <div class="back_arrow">
       <span @click="$emit('CloseModal')" class="material-symbols-outlined icon-colour">arrow_back_ios</span>
     </div>
@@ -44,9 +46,8 @@ defineProps({
   position: absolute;
   background-color: white;
   width: 100%;
-  top: 73px;
+  top: 0;
   z-index: 10;
-  padding: 2em 10%;
   bottom: 0;
 }
 
@@ -76,6 +77,16 @@ defineProps({
 .material-symbols-outlined:hover {
   transform: scale(1.2);
 }
+
+.blur {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 998;
+  background: rgba(0, 0, 0, 0.3);
+}
 </style>
 
 <script>
@@ -83,12 +94,45 @@ export default {
   data() {
     return {
       ModalIsOpen: true,
+      my_information: {
+        first_name: "",
+        last_name: "",
+        email: "",
+        phone_number: "",
+        location: "",
+      },
+      my_profile: {
+        seller_image: "",
+        seller_name: "",
+        description: "",
+      },
+      listings: {
+        my_listings: [],
+      },
     };
   },
 
   methods: {
     CloseModal() {
       this.ModalIsOpen = false;
+    },
+    async update_user(userID){
+            const response = await fetch("http://localhost:4000/users/update/"+this.logged_userID, {
+                method:"PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(this.user_body_data)
+        });
+        const received_data = await response.json();     
+        },
+    updateInfo(){
+      
+    },
+    updateProfile(){
+      this.listings.my_listings = this.logged_user_obj.my_listings;
+      this.update_user(this.logged_userID);
+    },
+    updateListings(){
+      
     },
   },
 };
