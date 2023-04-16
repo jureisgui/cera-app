70c8794769d72e8ea533f6345d3fed6a3186a608
jureisgui <111708095+jureisgui@users.noreply.github.com>
2023-04-16 14:20:11 +1200

    started methods, but it's in the wrong component.
    Commented methods, so we can try at a later point if we want to

--- front-end/src/components/Modals/ProductModal/QA.vue
+++ front-end/src/components/Modals/ProductModal/QA.vue
@@ -50,3 +50,49 @@ defineProps({
   },
 });
 </script>
+
+// <script>
+// export default {
+//   data(){
+//     return{
+//     user_body_data: {
+//         first_name: "",
+//         last_name: "",
+//         },    
+//     logged_userID:'',
+//     question_body_data:{
+//       question_input:'',
+//       product_id:'',
+//     }
+    
+//         }   
+//   },
+//   methods: {
+//     async fetch_single_user(userID){
+//         const response = await fetch("http://localhost:4000/users/getuser/"+userID);
+//         const received_data = await response.json();
+//         this.user_body_data=received_data;
+//     },
+//     async create_question(){
+//       const response = await fetch("http://localhost:4000/users/addquestion", {
+//         method:"POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify(this.question_body_data)
+//       });
+//         const received_data = await response.json();
+//         }    
+//     },
+//   created(){
+//     this.logged_userID=localStorage.getItem("logged_userID")
+//     this.fetch_single_user(this.logged_userID)
+//   },
+//   computed:{
+        
+//     initials(){
+//       return this.first_name[0]+this.first_name[0]
+//     }
+//   }
+// }
+  
+
+// </script>
